5c8bcadd348d414003456a671c92c4bf7095f749
Pedro Pinheiro <pmop@protonmail.com>
2024-04-24 02:28:18 -0300

    Change to sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -2,26 +2,22 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_17_213559) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
+ActiveRecord::Schema[7.1].define(version: 2020_05_17_213559) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
     t.string "record_type", null: false
     t.integer "record_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
   end
 
@@ -30,7 +26,7 @@ ActiveRecord::Schema.define(version: 2020_05_17_213559) do
     t.string "record_type", null: false
     t.integer "record_id", null: false
     t.integer "blob_id", null: false
-    t.datetime "created_at", null: false
+    t.datetime "created_at", precision: nil, null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
@@ -42,34 +38,31 @@ ActiveRecord::Schema.define(version: 2020_05_17_213559) do
     t.text "metadata"
     t.bigint "byte_size", null: false
     t.string "checksum", null: false
-    t.datetime "created_at", null: false
+    t.datetime "created_at", precision: nil, null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "comments", force: :cascade do |t|
-    t.bigint "post_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.integer "post_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table "comments_posts", id: false, force: :cascade do |t|
-    t.bigint "comment_id", null: false
-    t.bigint "post_id", null: false
-    t.index ["comment_id"], name: "index_comments_posts_on_comment_id"
-    t.index ["post_id"], name: "index_comments_posts_on_post_id"
-  end
-
   create_table "posts", force: :cascade do |t|
     t.integer "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.integer "tags_id", null: false
+    t.text "content"
+    t.string "content_type", limit: 40
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "title", limit: 255
     t.string "permalink", limit: 255
     t.text "summary"
     t.index ["permalink"], name: "index_posts_on_permalink"
+    t.index ["tags_id"], name: "index_posts_on_tags_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -91,22 +84,24 @@ ActiveRecord::Schema.define(version: 2020_05_17_213559) do
 
   create_table "tags", force: :cascade do |t|
     t.string "name", limit: 30
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.integer "posts_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "permalink", limit: 255
     t.index ["permalink"], name: "index_tags_on_permalink"
+    t.index ["posts_id"], name: "index_tags_on_posts_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name", limit: 50, null: false
     t.integer "posts_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "email", limit: 254, default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
     t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["posts_id"], name: "index_users_on_posts_id"
@@ -116,6 +111,8 @@ ActiveRecord::Schema.define(version: 2020_05_17_213559) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
+  add_foreign_key "posts", "tags", column: "tags_id"
   add_foreign_key "posts", "users"
+  add_foreign_key "tags", "posts", column: "posts_id"
   add_foreign_key "users", "posts", column: "posts_id"
 end
