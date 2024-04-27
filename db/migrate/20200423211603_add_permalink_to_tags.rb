3ac5f63225113bcf94b4e481f3b125a6ff8df636
Pedro Pinheiro <pmop@protonmail.com>
2024-04-26 21:18:53 -0300

    Add Rspec

--- db/migrate/20200423211603_add_permalink_to_tags.rb
+++
@@ -1,6 +0,0 @@
-class AddPermalinkToTags < ActiveRecord::Migration[6.0]
-  def change
-    add_column :tags, :permalink, :string, limit: 255
-    add_index :tags, :permalink
-  end
-end
--- db/migrate/20200506092219_remove_comments_and_commments_area.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCommentsAndCommmentsArea < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :posts, :comment_area_id
-  end
-end
