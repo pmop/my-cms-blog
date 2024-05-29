a296f4ebe6d884e496d1cdbaac95e4d4fca679d3
Pedro Pinheiro <pmop@protonmail.com>
2024-05-29 19:24:38 -0300

    Cleanup

--- app/controllers/home_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class HomeController < ApplicationController
-  def index
-    # TODO: optimize queries
-    @posts = Post.with_rich_text_content.first(4)
-    @tags = Tag.all
-  end
-end
